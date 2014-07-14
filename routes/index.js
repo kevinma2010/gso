54a172fb585559fc7cefbe3cdd58b28167ff4d18
Lenbo Ma <mlongbo@gmail.com>
2014-07-14 12:39:09 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var gsearch = require('../lib/gsearch');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/search', function (req, res) {
+    var q = req.query.q;
+    gsearch(q,0, function (arr) {
+        res.setHeader('content-type','application/json; charset=UTF-8');//响应编码，如果是html,写在head中也可以
+        res.json(arr);
+    });
+});
+
+module.exports = router;
