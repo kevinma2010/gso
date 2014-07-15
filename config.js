8b7e37d2752f488626f2c8ee7cbcd0b0163e07b1
Lenbo Ma <mlongbo@gmail.com>
2014-07-15 13:46:31 +0800

    增加移动端访问检测

--- config.js
+++ config.js
@@ -4,5 +4,7 @@
 module.exports = {
     port: 5555,
     language: 'en',
+    userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' +
+        '(KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36',
     g_url: 'https://www.google.com.hk'
 };
