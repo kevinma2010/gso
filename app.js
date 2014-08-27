1be3fd70dbb65b6c671c584ade5e997e7d4ce006
Lenbo Ma <mlongbo@gmail.com>
2014-08-28 00:14:00 +0800

    更改500页面配置

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render('500', {
             message: err.message,
             error: err
         });
@@ -51,7 +51,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('500', {
         message: err.message,
         error: {}
     });
