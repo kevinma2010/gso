942b76f56bc53e777734d790f55a93a64768c753
Lenbo Ma <mlongbo@gmail.com>
2014-08-30 16:00:02 +0800

    测试

---
+++ test.js
@@ -0,0 +1,10 @@
+
+var gsearch = require('./lib/gsearch');
+
+var start = new Date().getTime();
+console.log(start);
+gsearch({q: 'java'}, function (result) {
+	var end = new Date().getTime();
+	console.log(end-start);
+	console.log(result.data.length);
+});
