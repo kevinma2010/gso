7e895f29cb9077511eb7c52422155a9497f55071
Lenbo Ma <mlongbo@gmail.com>
2014-09-07 15:56:31 +0800

    去除favicon()

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon());
+// app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
