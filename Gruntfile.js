6e3f43fbff4f652dd33d0ac5a3c4253f2183ca31
Lenbo Ma <mlongbo@gmail.com>
2014-11-15 01:14:51 +0800

    update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,23 @@
 module.exports = function (grunt) {
 
+	var static_dir = 'build/';
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		copy: {
+			statics: {
+				files: [
+					{
+						expand: true, 
+						src: ['public/images/**'], 
+						dest: static_dir
+					},
+					{
+						src: ['public/*'],
+						dest: static_dir,
+						filter: 'isFile'
+					}
+				]
+			},
 			main: {
 				files: [
 					{
@@ -35,9 +50,9 @@ module.exports = function (grunt) {
 				dest: 'build/config.js',
 				options: {
 					process: function (content, srcpath) {
-						content = content.replace(/r_prefix: ""/g, 'r_prefix: "//gusou.qiniudn.com"');
+						// content = content.replace(/r_prefix: ""/g, 'r_prefix: "//gusou.qiniudn.com"');
 						content = content.replace(/'en'/g, '\'zh-CN\'');
-						content = content.replace(/'https:\/\/www.google.com.hk'/g, '\'http:\/\/74.125.235.68\'');
+						content = content.replace(/'https:\/\/www.google.com.hk'/g, '\'http:\/\/74.125.204.94\'');
 						return content;
 					}
 				}
@@ -49,7 +64,7 @@ module.exports = function (grunt) {
 					expand: true,
 					cwd: 'public/stylesheets/',
 					src: ['*.css', '!*.min.css'],
-					dest: 'build/public/stylesheets/'
+					dest: static_dir+'public/stylesheets/'
 				}]
 			}
 		},
@@ -60,12 +75,13 @@ module.exports = function (grunt) {
 					expand: true,
 					cwd: 'public/javascripts/',
 					src: '**/*.js',
-					dest: 'build/public/javascripts/'
+					dest: static_dir+'/public/javascripts/'
 				}]
 			}
 		},
 		clean: {
-			build: ['build']
+			build: ['build'],
+			statics: [static_dir+'/public']
 		}
 	});
 
@@ -73,6 +89,8 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('static',['clean:statics', 'cssmin', 'uglify', 'copy:statics']);
 
 	grunt.registerTask('default', ['clean', 'cssmin', 'uglify', 'copy']);
 };
