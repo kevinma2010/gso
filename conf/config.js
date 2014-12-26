e3f6e60599e43111a1d49cb8f2e117baa5f2f95a
Lenbo Ma <mlongbo@gmail.com>
2014-12-26 22:09:54 +0800

    更改google ip配置

--- conf/config.js
+++ conf/config.js
@@ -17,8 +17,7 @@ config = {
         r_prefix: "", //https下的资源前缀
         key: path.join(__dirname, 'test_server.key'),
         cert: path.join(__dirname, 'test_server.crt')
-    },
-    ipArray: ['58.123.102.99','173.194.121.28','209.116.186.219']//google ip
+    }
 };
 
 module.exports = config;
