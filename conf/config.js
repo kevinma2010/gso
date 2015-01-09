fc7e9b775875259aa6fd3d7cc6f6a394c1fefab9
Lenbo Ma <mlongbo@gmail.com>
2015-01-09 23:08:04 +0800

    完善注释

--- conf/config.js
+++ conf/config.js
@@ -12,12 +12,12 @@ config = {
     userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' +
         '(KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36',
     r_prefix: "",//http下的资源前缀
-    autocomplate_url: '//173.194.121.28',
+    autocomplate_url: '//173.194.121.28', //http下的autocomplate请求地址(必须为没有被墙的IP)
     ssl: {
         enable: false,//设置https是否启用
-        port: 5556,
+        port: 5556, //https 端口
         r_prefix: "", //https下的资源前缀
-        autocomplate_url: '//wen.lu',
+        autocomplate_url: '//wen.lu', //https下的autocomplate请求地址
         key: path.join(__dirname, 'test_server.key'),
         cert: path.join(__dirname, 'test_server.crt')
     },
