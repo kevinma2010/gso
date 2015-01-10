5a79d3a63bad04b08f9bb4cb34667bef5944379c
Lenbo Ma <mlongbo@gmail.com>
2015-01-11 01:11:47 +0800

    增加打印信息

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,6 +6,9 @@
             var isInstalled = 0;
             try {
                isInstalled = _external.IsSearchProviderInstalled('https://gusouk.com');
+                if (isInstalled > 0) {
+                    alert("已添加过此搜索引擎");
+                }
             } catch (err) {
               isInstalled = 0;
             }
@@ -34,4 +37,5 @@
 
 console.info("谷搜客基于Google搜索,为喜爱谷歌搜索的朋友们免费提供高速稳定的搜索服务。搜索结果通过Google.com实时抓取，欢迎您在日常生活学习中使用谷搜客查询资料。");
 console.info("如果你觉得谷搜客对你有帮助，请资助(支付宝账号: mlongbo@gmail.com)我一下，资金将用于支持我开发及购买服务器资源。^_^");
-console.info("希望能够让更多的获取知识，各自加油！");
+console.info("本站使用开源项目gso(https://github.com/lenbo-ma/gso)搭建");
+console.info("希望能够让更多的获取知识，加油！");
